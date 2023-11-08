33fc51e02e196aca58d6ac837fd31fea06cfab31
Fernando Dingler <fdingler@amazon.com>
2023-11-07 16:00:13 -0800

    init

---
+++ postbuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BUILD_OUTPUT_DIR=".amplify-hosting"
+COMPUTE_OUTPUT_DIR=${BUILD_OUTPUT_DIR}/compute
+STATIC_OUTPUT_DIR=${BUILD_OUTPUT_DIR}/static
+
+rm -rf ./${BUILD_OUTPUT_DIR}
+mkdir -p ./${COMPUTE_OUTPUT_DIR}
+mkdir -p ./${STATIC_OUTPUT_DIR}
+
+cp -r build/ ./${COMPUTE_OUTPUT_DIR}/
+cp -r build/client ./${STATIC_OUTPUT_DIR}/
+cp ./deploy-manifest.json ./$BUILD_OUTPUT_DIR/
